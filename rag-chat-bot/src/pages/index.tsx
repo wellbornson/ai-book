20d75cf83ac59a429cfd8f364e5882075fb2d747
wellbornson <muhammad.zahid.imam@gmail.com>
2025-12-22 15:57:55 +0500

    Update homepage button text

--- rag-chat-bot/src/pages/index.tsx
+++ rag-chat-bot/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            AI BOOK CREATED BY ZAHID IMAM
           </Link>
         </div>
       </div>
